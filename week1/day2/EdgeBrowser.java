249c0a7bed1276374ff930141ce225db377bd731
esaravanan7 <sara.ven2004@gmail.com>
2025-06-22 19:53:31 +0530

    Add files via upload
    Week1 Day2

---
+++ week1/day2/EdgeBrowser.java
@@ -0,0 +1,11 @@
+package week1.day2;
+
+public class EdgeBrowser {
+
+	public static void main(String[] args) {
+		Browser brw = new Browser();
+		brw.launchBrowser("Firefox");
+		brw.loadUrl();
+	}
+
+}
---
+++ week1/day2/OddNumber.java
@@ -0,0 +1,17 @@
+package week1.day2;
+
+public class OddNumber {
+
+	public static void main(String[] args) {
+		for(int i=1; i<=10; i++) {
+			if (i%2!=0) {
+				System.out.println("Odd Number from 1 to 10 is :" +i);
+				}
+			else {
+				System.out.println("Even Number from 1 to 10 is :" +i);
+		}
+
+	}
+
+}
+}
