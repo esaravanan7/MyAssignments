c734874f308c6473be718e5bc2779e182dc7ab99
esaravanan7 <sara.ven2004@gmail.com>
2025-06-26 15:42:17 +0530

    Add files via upload
    Check number is positive

---
+++ HomeAssignments/CheckNumberIsPositive.java
@@ -0,0 +1,20 @@
+package HomeAssignments;
+
+public class CheckNumberIsPositive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int version = 10;
+
+        // Check if the number is positive or negative
+        if (version > 0) {
+            System.out.println(version +" is Positive");
+        } else if (version < 0) {
+            System.out.println(version +" is Negative");
+        } else {
+            System.out.println(version +" is Zero");
+        }
+    
+	}
+
+}
